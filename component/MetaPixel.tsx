0be8b2ecf360f41590698b55d3f5ae37a70c560b
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2026-05-01 13:24:28 +0530

    fix a bug

--- component/MetaPixel.tsx
+++ component/MetaPixel.tsx
@@ -18,7 +18,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', process.env.NEXT_PUBLIC_META_PIXEL_ID);
+fbq('init', ${process.env.NEXT_PUBLIC_META_PIXEL_ID});
 fbq('track', 'PageView');
   `,
         }}
